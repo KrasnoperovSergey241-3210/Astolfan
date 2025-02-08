17635f5de8dbc29e3913fa65babfdaf0aff25e89
KrasnoperovSergey241-3210 <sakrasnoperov2006@gmail.com>
2025-02-08 11:56:30 +0300

    First commit

--- Project/index.php
+++ Project/index.php
@@ -5,6 +5,12 @@
         public $weight;
         private $type;
         public $height;
+        function __construct(string $name, string $color, int $weight, string $height) {
+            $this->name = $name;
+            $this->color = $color;
+            $this->weight = $weight;
+            $this->height = $height;
+        }
         function setHeight(string $height) {
             $this->height = $height;
         }
@@ -18,10 +24,11 @@ $cat1->name = 'Barsik';
 $cat1->color = 'grey';
 $cat1->weight = 7;
 $cat1->height = setHeight("100");
-$cat1->height = setHeight("120");
+$cat2->height = setHeight("120");
 $cat2->name = 'Tishka';
-$cat1->color = 'red';
-$cat1->weight = 5;
+$cat2->color = 'red';
+$cat2->weight = 5;
+$cat3 = new Cat('Murka', 'black', 7, "80");
 echo $cat1->getHeight().'<BR>';
-echo $cat1->getHeight().'<BR>';
+echo $cat2->getHeight().'<BR>';
 var_dump($cat1);
