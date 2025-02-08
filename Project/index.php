e59ef6edb8a88f070feabacafc75f8aec4ebf5d7
KrasnoperovSergey241-3210 <sakrasnoperov2006@gmail.com>
2025-02-08 11:50:29 +0300

    First commit

--- Project/index.php
+++ Project/index.php
@@ -3,10 +3,25 @@
         public $name;
         public $color;
         public $weight;
+        private $type;
+        public $height;
+        function setHeight(string $height) {
+            $this->height = $height;
+        }
+        function getHeight() {
+            return $this->height;
+        }
     }
 
 $cat1 = new Cat();
 $cat1->name = 'Barsik'; 
 $cat1->color = 'grey';
 $cat1->weight = 7;
+$cat1->height = setHeight("100");
+$cat1->height = setHeight("120");
+$cat2->name = 'Tishka';
+$cat1->color = 'red';
+$cat1->weight = 5;
+echo $cat1->getHeight().'<BR>';
+echo $cat1->getHeight().'<BR>';
 var_dump($cat1);
