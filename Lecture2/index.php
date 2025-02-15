8e26f610a850160c8524987340306a40ca55ede1
KrasnoperovSergey241-3210 <sakrasnoperov2006@gmail.com>
2025-02-15 09:26:25 +0300

    Commit

--- Lecture2/index.php
+++ Lecture2/index.php
@@ -3,4 +3,17 @@
     for ($i = 0; $i < sizeof($arr); $i++) {
         echo $arr[$i].' ';
     }
+
+    $arr2['a'] = 2;
+    $arr2['b'] = 'r';
+    $arr2['c'] = 6;
+    $arr2['d'] = 8;
+    echo $arr2[2].'<BR>';
+
+    var_dump($arr2);
+    echo '<BR>';
+
+    for ($i = 0; $i < sizeof($arr2); $i++) {
+        echo $arr2[$i].' ';
+    }
 ?>
